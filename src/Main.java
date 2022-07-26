c8f28ce96d355f44f973c49117cf7cc86e37d186
luizfernando03 <luizfernandodemoura.2013@gmail.com>
2022-07-25 23:53:47 -0300

    Triangulo commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double xA, xB, xC,  yA, yB, yC;
+
+        System.out.println("Digite a medida do triangulo x; ");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+        System.out.println("Digite a medida do triangulo y; ");
+        yA = sc.nextDouble();
+        yB= sc.nextDouble();
+        yC = sc.nextDouble();
+
+        double p = (xA + xB + xC) / 2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC) / 2.0;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        System.out.printf("A area do triangulo X é : %.4f%n", areaX );
+        System.out.printf("Area do triangulo Y : %.4f%n", areaY);
+
+        if(areaX > areaY){
+            System.out.println("A largura da area é : X");
+        }
+        else {
+            System.out.println("A largura da area é : Y");
+
+        }
+
+        sc.close();
+    }
+}
